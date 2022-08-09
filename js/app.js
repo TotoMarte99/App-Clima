dae706581f923207c5a3594a4f92c0cd051e0ce5
Tomas <tomascmartelon@gmail.com>
2022-08-09 01:10:58 -0300

    Aplicacion API

--- js/app.js
+++ js/app.js
@@ -1,167 +1,177 @@
 //Imports de Main//
-import { ClimaParaMañana, ClimaPMañana, ClimaHoy} from "./main.js";
+import { ClimaParaMañana, ClimaPMañana, ClimaHoy } from "./main.js";
 
 //Declaracion de selectores//
-let botonCelcius = document.getElementById("boton_celcius");
-botonCelcius.addEventListener("click", cambiarAcelcius);
-let botonFarenheit = document.getElementById("boton_farenheit");
-botonFarenheit.addEventListener("click", cambiarAfarenheit);
 
-//Cambio de fondos//
-let cambio1 = document.querySelector("#climaMañana");
-cambio1.addEventListener("click", cambioAlluvia);
-cambio1.style.cursor = "pointer";
-var viento = document.querySelector("#viento");
-viento.style.color = "red";
-let cambio2 = document.querySelector("#climaPasadoMañana");
-cambio2.addEventListener("click", cambioAsoleado);
-cambio2.style.cursor = "pointer";
-let cambio3 = document.querySelector("#Clima");
-cambio3.addEventListener("click", cambioHoy);
-cambio3.style.cursor = "pointer";
+var buscadorApp = document.getElementById("subirInfo");
+buscadorApp.addEventListener("click", subir_info);
+
+let ciudad = document.getElementById("buscarCiudad");
+let pais = document.getElementById("buscarPais");
 
 //Fecha//
-const DateTime = luxon.DateTime
+const DateTime = luxon.DateTime;
 const dia = document.getElementById("fecha_1");
+dia.style.padding = "0px";
 dia.style.fontWeight = "700";
-dia.style.textDecoration = "underline";
-dia.style.color = "white"; 
+dia.style.filter = "filter: drop-shadow(16px 16px 20px orange)";
+dia.style.color = "white";
 const dt = DateTime.now();
-dt.year
-dt.month   
-dt.day     
+dt.year;
+dt.month;
+dt.day;
+dia.innerHTML = `Today ${dt.day}/${dt.month}/${dt.year}`;
 
-dia.innerHTML = `Hoy ${dt.day}/${dt.month}/${dt.year}`;
+//Envio de datos//
+function subir_info(e) {
+  e.preventDefault();
 
+  if (ciudad.value === "" || pais.value === "") {
+    mostrarError("Ambos campos son obligatorios...");
+    return;
+  }
 
-function cambioAlluvia(){
-    var fondo = document.querySelector("#body");
-    fondo.style.backgroundImage = "url('./img/pexels-genaro-servín-763398.jpg')";
-    fondo.style.backgroundSize = "cover";
-    fondo.style.height = "100vh"
-    document.querySelector("#nubosidad").innerHTML = `${ClimaParaMañana[0].nubosidad}`
-    document.querySelector("#humedad").innerHTML = `${ClimaParaMañana[0].humedad}`
-    document.querySelector("#viento").innerHTML = `${ClimaParaMañana[0].viento}`
-    const DateTime = luxon.DateTime
-    const dia = document.getElementById("fecha_1");
-    dia.style.fontWeight = "700";
-    dia.style.textDecoration = "underline";
-    dia.style.color = "white"; 
-    const dt = DateTime.now();
-    dt.year
-    dt.month   
-    dt.day     
-    dia.innerHTML = `Domingo ${dt.day + 1}/${dt.month}/${dt.year}`;
-
-    //Operador Ternario//
-    ClimaParaMañana[0].viento < '30' ? viento.style.color = "white" : "red";
-    
-} 
-
-function cambioAsoleado(){
-    var fondo = document.querySelector("#body");
-    fondo.style.backgroundImage = "url('./img/pexels-simon-berger-1353126.jpg')";
-    fondo.style.backgroundSize = "cover";
-    fondo.style.height = "100vh"
-    document.querySelector("#nubosidad").innerHTML = `${ClimaPMañana[0].nubosidad}`
-    document.querySelector("#humedad").innerHTML = `${ClimaPMañana[0].humedad}`
-    document.querySelector("#viento").innerHTML = `${ClimaPMañana[0].viento}`
-    const dt = DateTime.now();
-    dt.year
-    dt.month   
-    dt.day     
-    dia.innerHTML = `Lunes ${dt.day + 2}/${dt.month}/${dt.year}`;
-    
-    //Operador Ternario//
-    ClimaPMañana[0].viento > '30' ? viento.style.color = "red" : "white";
+  llamadoAPI(ciudad.value, pais.value);
 }
 
-function cambioHoy(){
-    var fondo = document.querySelector("#body");
-    fondo.style.backgroundImage = "url('https://source.unsplash.com/1600x1000/?landscape')";
-    fondo.style.backgroundSize = "cover";
-    fondo.style.height = "100vh"
-    document.querySelector("#nubosidad").innerHTML = `${ClimaHoy[0].nubosidad}`
-    document.querySelector("#humedad").innerHTML = `${ClimaHoy[0].humedad}  `
-    document.querySelector("#viento").innerHTML = `${ClimaHoy[0].viento}`
-    const DateTime = luxon.DateTime
-    const dia = document.getElementById("fecha_1");
-    dia.style.fontWeight = "700";
-    dia.style.textDecoration = "underline";
-    dia.style.color = "white"; 
-    const dt = DateTime.now();
-    dt.year
-    dt.month   
-    dt.day     
-
-    dia.innerHTML = `Hoy ${dt.day}/${dt.month}/${dt.year}`;
-
-    //Operador Ternario//
-    ClimaHoy[0].viento > '30' ? viento.style.color = "red" : "white";
+function llamadoAPI(ciudad, pais) {
+  const apiId = "137e3e20b0b47894913292b4de216a3a";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiId}`;
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((dataJSON) => {
+      if (dataJSON.cod === "404") {
+        mostrarError("Ciudad no encontrada...");
+      } else {
+        mostrarClima(dataJSON);
+      }
+    });
 }
 
+let humedad = document.querySelector("#humedad");
+let vientoo = document.querySelector("#viento");
+let nubosidad = document.querySelector("#nubosidad");
+let ciudades = document.querySelector("#ciudades");
+let clima = document.querySelector("#temperatura_hoy");
+let min = document.querySelector("#min");
+let max = document.querySelector("#max");
+let descriptionn = document.querySelector("#banner_informativo");
+
+function mostrarClima(data) {
+  const { name } = data;
+  const { temp, humidity, temp_min, temp_max } = data.main;
+  const { speed } = data.wind;
+  const { all } = data.clouds;
+  const { description } = data.weather[0];
+
+  const tempC = KelvinToCentigrados(temp);
+  const minT = KelvinToCentigrados(temp_min);
+  const maxT = KelvinToCentigrados(temp_max);
+  const tempF = CentigradosToFarenheit(temp);
+  const minToF = MinToFarenheit(temp_min);
+  const maxToF = MaxToFarenheit(temp_max);
+
+  let btn = document.getElementById("F");
+  btn.addEventListener("click", cambiarFarenheit);
+  function cambiarFarenheit() {
+    document.querySelector("#temperatura_hoy").innerHTML = tempF + "°F";
+    document.querySelector("#min").innerHTML = `<small>Temp.Min</small> 
+    <h4>${minToF + "°F"}</h4>`;
+    document.querySelector("#max").innerHTML = `<small>Temp.Max</small> 
+    <h4>${maxToF + "°F"}</h4>`;
+  }
+
+  let btn2 = document.getElementById("C");
+  btn2.addEventListener("click", cambiarCelcius);
+  function cambiarCelcius() {
+    document.querySelector("#temperatura_hoy").innerHTML = tempC + "°C";
+    document.querySelector("#min").innerHTML = `<small>Temp.Min</small> 
+    <h4>${minT + "°C"}</h4>`;
+    document.querySelector("#max").innerHTML = `<small>Temp.Max</small> 
+    <h4>${maxT + "°C"}</h4>`;
+  }
+
+  humedad.innerHTML = `<h3>${humidity}%</h3>`;
+  vientoo.innerHTML = `<h3>${speed}Km/h</h3`;
+  nubosidad.innerHTML = `<h3>${all}%</h3>`;
+  ciudades.innerHTML = `<h2>${name}</h2>`;
+  clima.innerHTML = `<p>${tempC}°</p>`;
+  min.innerHTML = `
+  <small>Temp.Min</small> 
+  <h4>${minT}°</h4>`;
+  max.innerHTML = `
+  <small>Temp.Max</small> 
+  <h4>${maxT}°</h4>`;
+  descriptionn.innerHTML = description;
+
+  if (description === "clear sky") {
+    document.body.style.backgroundImage = "url('../img/despejado.jpg')";
+  } else if (description === "rain" || "shower rain" || "light rian") {
+    document.body.style.backgroundImage = "url('../img/lluvia.jpg')";
+  } else if (description === "light snow" || "light snow") {
+    document.body.style.backgroundImage = "url('../img/nieve.jpg')";
+  } else if (description === "thunderstorm") {
+    document.body.style.backgroundImage = "url('../img/tormenta.jpg')";
+  } else if (
+    description === "overcast clouds" ||
+    "few clouds" ||
+    "scattered clouds" ||
+    "broken clouds"
+  ) {
+    document.body.style.backgroundImage = "url('../img/nublado.jpg')";
+  }
+}
+
+function mostrarError(mensaje) {
+  const alerta = document.createElement("p");
+  alerta.style.setProperty("color", "red");
+  alerta.style.setProperty("text-align", "center");
+  alerta.style.setProperty("padding-top", "20px");
+  alerta.classList.add("alert-mensaje");
+  alerta.innerHTML = mensaje;
+  buscando.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 3000);
+}
 
 //Funciones de los eventos//
-function cambiarAcelcius() {
-    document.getElementById("Clima").innerHTML = `<p class="clima_hoy">Hoy 🌥️</p>
-    <h1>
-    ${ClimaHoy[0].temperatura + "C"}
-    </h1>`;
-    document.querySelector(
-    "#climaMañana"
-    ).innerHTML = `<p class="temperatura_mañana">Domingo ⛈️​ </p>
-    <h1>
-    ${ClimaParaMañana[0].temperatura + "C"}
-    </h1>`;
-    document.querySelector(
-    "#climaPasadoMañana"
-    ).innerHTML = `<p class="temperatura_mañana">Lunes 🌤️​</p>
-    <h1>
-    ${ClimaPMañana[0].temperatura + "C"}
-    </h1>`;
+function CentigradosToFarenheit(temp) {
+  return parseInt(((temp - 273.15) * 9) / 5 + 32);
+}
+function MinToFarenheit(temp_min) {
+  return parseInt(((temp_min - 273.15) * 9) / 5 + 32);
+}
+function MaxToFarenheit(temp_max) {
+  return parseInt(((temp_max - 273.15) * 9) / 5 + 32);
 }
 
-function cambiarAfarenheit() {
-    document.getElementById("Clima").innerHTML = `<p class="clima_hoy">Hoy 🌥️ </p>
-    <h1>
-    ${(24 * 9) / 5 + 32 + "°F"}
-    </h1>`;
-    document.querySelector(
-    "#climaMañana"
-    ).innerHTML = `<p class="temperatura_mañana">Domingo ⛈️​ </p>
-    <h1>
-    ${(15 * 9) / 5 + 32 + "°F"}
-    </h1>`;
-    document.querySelector(
-    "#climaPasadoMañana"
-    ).innerHTML = `<p class="temperatura_mañana">Lunes 🌤️​</p>
-    <h1>
-    ${(9 * 9) / 5 + 32 + "°F"}
-    </h1>`;
+function KelvinToCentigrados(temp) {
+  return parseInt(temp - 273.15);
 }
 
 //Uso de for para recorrer el array y brindar por consola una advertencia sobre los altos vientos//
 for (const viento of ClimaPMañana) {
-    console.log(
-    "Advertencia: hoy el viento es de " +
-    viento.viento +
-    " Recomendamos suspender actividades nauticas"
-    );
+  console.log(
+    "Warning: today the wind is to strong " +
+      viento.viento +
+      " We recommend taking care, and suspend nautical activities "
+  );
 }
 
-
 //Uso de Metodos de busqueda sobre el Array//
-const lluviaHoy = ClimaHoy.some(
-    (ClimaHoy)=> ClimaHoy.lluvia === "llueve"
-);
-console.log(lluviaHoy + ": Hoy no va a necesitar paraguas");
+const lluviaHoy = ClimaHoy.some((ClimaHoy) => ClimaHoy.lluvia === "llueve");
+console.log(lluviaHoy + ": You don't need a unbrella today");
 
 const lluvia = ClimaParaMañana.some(
-    (ClimaParaMañana) => ClimaParaMañana.lluvia === "llueve"
+  (ClimaParaMañana) => ClimaParaMañana.lluvia === "llueve"
 );
-console.log(lluvia + ": Hoy no va a necesitar paraguas");
+console.log(lluvia + ": You don't need a unbrella today");
 
 const lluviaPmañana = ClimaPMañana.some(
-    (ClimaPMañana) => ClimaPMañana.lluvia === "llueve"
+  (ClimaPMañana) => ClimaPMañana.lluvia === "llueve"
 );
-console.log(lluviaPmañana + ": Recuerde mañana, llevar paraguas");
+console.log(lluviaPmañana + ": Remember, take your umbrella before go outside");
--- js/main.js
+++ js/main.js
@@ -3,51 +3,51 @@ localStorage.setItem('Ubicacion', 'Rosario');
 
 
 class ClimaMañana {
-    constructor(humedad, viento, nubosidad, lluvia, temperatura,id) {
-        this.humedad = humedad;
-        this.viento = viento;
-        this.nubosidad = nubosidad;
-        this.lluvia = lluvia;
-        this.temperatura = temperatura;
+    constructor(Humidity , Wind, Clouds, Rain, Temperature,id) {
+        this.Humidity = Humidity;
+        this.Wind = Wind;
+        this.Clouds = Clouds;
+        this.Rain = Rain;
+        this.Temperature = Temperature;
         this.id = id;
     }
 }
 class ClimaPasadoMañana {
-    constructor(humedad, viento, nubosidad, lluvia, temperatura,id) {
-        this.humedad = humedad;
-        this.viento = viento;
-        this.nubosidad = nubosidad;
-        this.lluvia = lluvia;
-        this.temperatura = temperatura;
+    constructor(Humidity , Wind, Clouds, Rain, Temperature,id) {
+        this.Humidity = Humidity;
+        this.Wind = Wind;
+        this.Clouds = Clouds;
+        this.Rain = Rain;
+        this.Temperature = Temperature;
         this.id = id;
     }
 }
 
 class ClimaDeHoy {
-    constructor(humedad, viento, nubosidad, lluvia, temperatura,id) {
-        this.humedad = humedad;
-        this.viento = viento;
-        this.nubosidad = nubosidad;
-        this.lluvia = lluvia;
-        this.temperatura = temperatura;
+    constructor(Humidity , Wind, Clouds, Rain, Temperature,id) {
+        this.Humidity = Humidity;
+        this.Wind = Wind;
+        this.Clouds = Clouds;
+        this.Rain = Rain;
+        this.Temperature = Temperature;
         this.id = id;
     }
 }
 const ClimaParaMañana = [];
 ClimaParaMañana.push(
-    new ClimaMañana("15%", "2Km/h", "19%", "llueve", "15°",'Clima Para Mañana')
+    new ClimaMañana("15%", "2Km/h", "19%", "it rains", "15°",'Clima Para Mañana')
 );
 localStorage.setItem("Clima para mañana", JSON.stringify(ClimaParaMañana));
 
 const ClimaPMañana = [];
 ClimaPMañana.push(
-    new ClimaPasadoMañana("0.2%", "35Km/h", "0.1%", "No llueve", "9°",'Clima Pasado Mañana')
+    new ClimaPasadoMañana("0.2%", "35Km/h", "0.1%", "it doesn't rain", "9°",'Clima Pasado Mañana')
 );
 localStorage.setItem("Clima Pasado Mañana", JSON.stringify(ClimaPMañana));
 
 const ClimaHoy = [];
 ClimaHoy.push(
-    new ClimaDeHoy("78%", "33Km/h", "25%", "No llueve", "24°",'Clima de Hoy'));
+    new ClimaDeHoy("78%", "33Km/h", "25%", "it doesn't rain", "24°",'Clima de Hoy'));
 localStorage.setItem("clima de Hoy", JSON.stringify(ClimaHoy));
 
 //Aplicacion de Spread//
